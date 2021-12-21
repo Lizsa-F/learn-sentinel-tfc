37001cfd07b509dd82d43e2162afc8dea2f87aba
Lisa Francis <64404058+Lizsa-F@users.noreply.github.com>
2021-12-21 15:45:14 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "TCS-terraform-learning"
+    organization = "TCS-Terraform-learning"
 
     workspaces {
       name = "sentinel-example"
